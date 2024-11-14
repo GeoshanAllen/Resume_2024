7a8d4606ca0533d0898228d7287b85de8640d8d5
GeoshanAllen <geoshanallen@gmail.com>
2024-11-15 03:55:51 +0530

    15Nov24  commit - routing added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import NavbarResume from "./components/Navbar";
 import Skills from "./components/Skills";
 
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,12 +13,12 @@ function App() {
     <div className="App">
       <div className="container"></div>
       <NavbarResume></NavbarResume>
-      <Router>
+      {/* <Router> */}
         <Routes>
-          <Route path ="Resume_2024/Skills/" element ={<Skills></Skills>} ></Route>
-          <Route path ="Resume_2024/Home/" element ={<Home></Home>} ></Route>
+          <Route path ="/Resume_2024/Skills/" element ={<Skills></Skills>} ></Route>
+          <Route path ="/Resume_2024/Home/" element ={<Home></Home>} ></Route>
         </Routes>
-      </Router>
+      {/* </Router> */}
       
       
       <Footer></Footer>
