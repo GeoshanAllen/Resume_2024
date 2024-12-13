ca608bb1904e9304598a1bec7be3c05e950d35bc
GeoshanAllen <geoshanallen@gmail.com>
2024-12-14 01:13:54 +0530

    Expience added 13Dec24 1

--- src/components/Accolades.js
+++ src/components/Accolades.js
@@ -1,56 +1,68 @@
 import React from "react";
-import Carousel from 'react-bootstrap/Carousel';
-import Carousel2 from '../../src/carousal/Committee.jpg';
-import Carousel1 from '../../src/carousal/DigitizationAward.jpg';
-import Carousel3 from '../../src/carousal/UltraRunner.jpg';
-import Carousel4 from '../../src/carousal/GuidingStar.jpg';
-import Carousel5 from '../../src/carousal/MavericAward.jpg';
-import Carousel6 from '../../src/carousal/SCBAward.jpg';
-
-
+import "../styles.css";
+import Carousel from "react-bootstrap/Carousel";
+import Carousel2 from "../../src/carousal/Committee.jpg";
+import Carousel1 from "../../src/carousal/DigitizationAward.jpg";
+import Carousel3 from "../../src/carousal/UltraRunner.jpg";
+import Carousel4 from "../../src/carousal/GuidingStar.jpg";
+import Carousel5 from "../../src/carousal/MavericAward.jpg";
+import Carousel6 from "../../src/carousal/SCBAward.jpg";
 
 export default function Accolades() {
-    return (
+  return (
+    <div>
+      <br></br>
 
-           
-        <Carousel fade  data-bs-theme="dark">
-          <Carousel.Item>
-          <img src={Carousel1} alt=""/>
-            <Carousel.Caption>
-              <h4>Standard Chartered: Digitization Award</h4>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img src={Carousel2} alt=""/>
-            <Carousel.Caption>
-            <h4>Standard Chartered: Employee Engagment Award</h4>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img src={Carousel3} alt=""/>
-            <Carousel.Caption>
-            <h4>Standard Chartered: Ultra Runner Award</h4>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img src={Carousel4} alt=""/>
-            <Carousel.Caption>
-            <h4>Maveric: Guiding Star Award</h4>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img src={Carousel5} alt=""/>
-            <Carousel.Caption>
-            <h4>Maveric: Recruitment Support Award</h4>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img src={Carousel6} alt=""/>
-            <Carousel.Caption>
-            <h4>Standard Chartered: High Performer Award</h4>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-  
-      );
+      <Carousel fade data-bs-theme="dark">
+        <Carousel.Item>
+          <img src={Carousel1} alt="" />
+          <Carousel.Caption>
+            <center>
+            <h4 class="bg-dark text-white" style={{ width: "50rem" }}>Standard Chartered: Digitization Award</h4>
+            </center>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Carousel2} alt="" />
+          <Carousel.Caption>
+          <center>
+            <h4 class="bg-dark text-white" style={{ width: "50rem" }}>Standard Chartered: Employee Engagment Award</h4>
+            </center>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Carousel3} alt="" />
+          <Carousel.Caption>
+          <center>
+            <h4 class="bg-dark text-white" style={{ width: "50rem" }}>Standard Chartered: Ultra Runner Award</h4>
+            </center>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Carousel4} alt="" />
+          <Carousel.Caption>
+          <center>
+            <h4 class="bg-dark text-white" style={{ width: "50rem" }}>Maveric: Guiding Star Award</h4>
+            </center>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Carousel5} alt="" />
+          <Carousel.Caption>
+          <center>
+            <h4  class="bg-dark text-white" style={{ width: "50rem" }}>Maveric: Recruitment Support Award</h4>
+            </center>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={Carousel6} alt="" />
+          <Carousel.Caption>
+          <center>
+            <h4 class="bg-dark text-white" style={{ width: "50rem" }}>Standard Chartered: High Performer Award</h4>
+            </center>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 }
